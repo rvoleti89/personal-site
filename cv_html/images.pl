16351d249d421160cd52ac422800ead0bb93318b
Rohit Voleti <rnvoleti@asu.edu>
2019-05-24 02:22:17 -0700

    updated all pages, but need to add research descriptions

---
+++ cv_html/images.pl
@@ -0,0 +1,169 @@
+# LaTeX2HTML 2019 (Released January 1, 2019)
+# Associate images original text with physical files.
+
+
+$key = q/sim;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 1.08ex; vertical-align: -0.12ex; " SRC="|."$dir".q|img1.svg"
+ ALT="$\sim$">|; 
+
+$key = q/{position}-Developedusecasesandrequirementsforhardwareandsoftwareprojectsinvolvispecialists-RequirementsandspecificationsmanagementinDOORSdatabase{position};AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img3.svg"
+ ALT="\begin{position}
+- Developed use cases and requirements for hardware and softwa...
+...
+- Requirements and specifications management in DOORS database
+\end{position}">|; 
+
+$key = q/{position}-ProvidedassistancetoahardwareengineeringresearchanddevelopmentteamindptuneandNaidaspeechprocessorsforsaleintheUnitedStatesandworldwide.{position};AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img4.svg"
+ ALT="\begin{position}
+- Provided assistance to a hardware engineering research and d...
+...a speech
+processors for sale in the United States and worldwide.
+\end{position}">|; 
+
+$key = q/{position}-emph{Jan`18-Present}:conductingresearchundersupervisor{Dr.VisarBerishnductingresearchonhuman-machineinteractionandcyber-physicalsystems{position};AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img2.svg"
+ ALT="\begin{position}
+- \emph{Jan \lq 18 - Present}: conducting research under \supervi...
+... research on human-machine interaction and cyber-physical systems
+\end{position}">|; 
+
+$key = q/{project}title{CourseReader-ElectricalEngineering103(AppliedNumericalComputing)}tit{AppliedNumericalComputing},taughtbyProfessorLievenVandenberghe}{project};AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img14.svg"
+ ALT="\begin{project}
+\title{Course Reader - Electrical Engineering 103 (Applied Nume...
+...d Numerical Computing}, taught by Professor Lieven Vandenberghe
+}
+\end{project}">|; 
+
+$key = q/{project}title{ECoGClassificationofFingerMovements}supervisor{Course:EEM255-NeurrdedEcOGtoclassifyfingermovementsforbrain-computerinterfacedesign.}{project};AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img16.svg"
+ ALT="\begin{project}
+\title{ECoG Classification of Finger Movements}
+\supervisor{Co...
+... classify finger movements for brain-computer interface design.
+}
+\end{project}">|; 
+
+$key = q/{project}title{EEGSignalProcessingforFlowExperimentsinHuman-MachineInteraction}sngwithamobiledevicetoanalyzetheuserexperiencebasedontaskdifficulty}{project};AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img8.svg"
+ ALT="\begin{project}
+\title{EEG Signal Processing for Flow Experiments in Human-Mach...
+... device to analyze the user experience based on task difficulty
+}
+\end{project}">|; 
+
+$key = q/{project}title{GreenPowerGenerationwithLowWindSpeedandPiezoelectricBeam}durationngadvantageoftheflutterofanaluminumbeamwithapiezoelectricmaterial.}{project};AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img9.svg"
+ ALT="\begin{project}
+\title{Green Power Generation with Low Wind Speed and Piezoelec...
+... the flutter of an aluminum beam with a piezoelectric material.
+}
+\end{project}">|; 
+
+$key = q/{project}title{LanguageAnalysisforPatientswithBipolarDisorderandSchizophrenia}suce,syntacticcomplexity,andexploredcorrelationwithclinicalvariables}{project};AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img6.svg"
+ ALT="\begin{project}
+\title{Language Analysis for Patients with Bipolar Disorder and...
+...ic complexity, and explored correlation with clinical variables
+}
+\end{project}">|; 
+
+$key = q/{project}title{LenslessUltra-widefieldCell-monitoringArrayplatformbasedonShadowiameraasacell-monitoringarrayplatformforinexpensivemedicaldiagnosis}{project};AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img10.svg"
+ ALT="\begin{project}
+\title{Lensless Ultra-wide field Cell-monitoring Array platform...
+...ell-monitoring array platform for inexpensive medical diagnosis
+}
+\end{project}">|; 
+
+$key = q/{project}title{MRIImageReconstruction}supervisor{Course:EE239AS-PrinciplesofMRIPk-space),usinginterpolationandgriddingtechniquestoreconstructdata.}{project};AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img17.svg"
+ ALT="\begin{project}
+\title{MRI Image Reconstruction}
+\supervisor{Course : EE 239AS...
+...sing interpolation and gridding techniques to reconstruct data.
+}
+\end{project}">|; 
+
+$key = q/{project}title{MotionTrackingforGaitAnalysiswithIMU}supervisor{SupervisedbyDr.UmandmagnetometerdatatransmittedoverBluetoothLow-Energy(BLE)protocol}{project};AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img7.svg"
+ ALT="\begin{project}
+\title{Motion Tracking for Gait Analysis with IMU}
+\supervisor...
+...meter data transmitted over Bluetooth Low-Energy (BLE) protocol
+}
+\end{project}">|; 
+
+$key = q/{project}title{NaturalLanguageInference}supervisor{Course:CSE576-NaturalLanguagelarchitecturetoimproveresultsindifficultsentencepairsfortheNLItask}{project};AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img15.svg"
+ ALT="\begin{project}
+\title{Natural Language Inference}
+\supervisor{Course : CSE 57...
+...to improve results in difficult sentence pairs for the NLI task
+}
+\end{project}">|; 
+
+$key = q/{project}title{PitchTrackingforNoisySpeech}supervisor{Course:EE214A-DigitalSpeecnitudedifferencefunction(AMDF),inbothnoisyandnoiselessenvironments}{project};AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img18.svg"
+ ALT="\begin{project}
+\title{Pitch Tracking for Noisy Speech}
+\supervisor{Course : E...
+...rence function (AMDF), in both noisy and noiseless environments
+}
+\end{project}">|; 
+
+$key = q/{project}title{SimulationofWordSubstitutionErrorsinAutomaticSpeechRecognition}suaskswhenerrorswereintroduced(seeICASSP-2019conferenceproceedings).}{project};AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img5.svg"
+ ALT="\begin{project}
+\title{Simulation of Word Substitution Errors in Automatic Spee...
+...rors were introduced (see ICASSP-2019 conference proceedings).
+}
+\end{project}">|; 
+
+$key = q/{project}title{TeachingAssistant-ElectricalEngineering110L(CircuitsLaboratory)}sudentstoelectroniclabequipmentandbasiccomponentsandinstrumentation}{project};AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img11.svg"
+ ALT="\begin{project}
+\title{Teaching Assistant - Electrical Engineering 110L (Circui...
+...ectronic lab equipment and basic components and instrumentation
+}
+\end{project}">|; 
+
+$key = q/{project}title{TeachingAssistant-Math33B(DifferentialEquations)}supervisor{UCLADithundergraduatestudentstakingdifferentialequations}{project};MSF=1.6;TAGS=R;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img12.svg"
+ ALT="\begin{project}
+\title{Teaching Assistant - Math 33B (Differential Equations)}
+...
+...tions with undergraduate students taking differential equations
+}
+\end{project}">|; 
+
+$key = q/{project}title{TeachingAssistant-Math3B(CalculusII)}supervisor{UCLADepartmentofMduatestudentstakingCalculusIIforstudentsstudyingbiologicalsciences}{project};AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img13.svg"
+ ALT="\begin{project}
+\title{Teaching Assistant - Math 3B (Calculus II)}
+\supervisor{U...
+...nts taking Calculus II for students studying biological sciences
+}
+\end{project}">|; 
+
+1;
+
